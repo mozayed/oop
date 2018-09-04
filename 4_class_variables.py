b65c96e416690e05df481559c610eb04f48fc7c1
Mohamed Zayed <m_zayed@hotmail.com>
2018-09-04 13:36:12 -0400

    Adding classvariables to the class

---
+++ 4_class_variables.py
@@ -0,0 +1,24 @@
+# Class Variables
+
+class Employees:
+    
+    raise_amount = 1.04
+    
+    def __init__(self, first, last, pay):
+        self.first = first #i can use self.anyname but we like to stick to argument name
+        self.last = last
+        self.pay = pay
+        self.email = first + '.' + last + '@mycompany.com'
+     
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+    
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amount) # we can use Employees.raise_amount
+
+emp_1 = Employees('John','Smith',80000)
+emp_2 = Employees('Sally','Johnson',45000)
+
+print(Employees.raise_amount)
+print(emp_1.raise_amount)
+print(emp_2.raise_amount)
